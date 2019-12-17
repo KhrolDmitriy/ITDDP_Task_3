626662d0f2d8dab1e9c17c6cfc2f74bd44db3a6b
KhrolDmitriy <44709141+KhrolDmitriy@users.noreply.github.com>
2019-12-17 09:35:19 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Client_v3.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client_v3
+{
+    public partial class MainWindow : Window
+    {
+        public string nickName { get; set; }
+        IConnection conn{ get; set; }
+        // Создание и конфигурирование подключения к очереди
+        private IModel GetRabbitChannel(string exchangeName, string queueName, string routingKey)
+        {
+            IModel model = conn.CreateModel();
+            model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            model.QueueDeclare(queueName, false, false, false, null);
+            model.QueueBind(queueName, exchangeName, routingKey, null);
+            return model;
+        }
+
+        // Соединение с броккером
+        private IConnection GetRabbitConnection()
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            IConnection conn = factory.CreateConnection();
+            return conn;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // нажатие кнопки Авторизация
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(Login.Text))
+                return;
+            nickName = Login.Text;
+            ButtonLogin.IsEnabled = false;
+            Login.IsEnabled = false;
+            conn = GetRabbitConnection();
+            SendMessage(new Message() { Login = nickName, Text = "Authorization" });
+            UpdateWall();
+            ButtonLogin.Visibility = Visibility.Hidden;
+            Login.IsEnabled = false;
+            Wall.Visibility = Visibility.Visible;
+            SendMessageBt.Visibility = Visibility.Visible;
+            Message.Visibility = Visibility.Visible;
+        }
+        async Task UpdateWall()
+        {
+            await Task.Run(() =>
+            {
+                IModel model = GetRabbitChannel(nickName, nickName, nickName);
+                var subscription = new Subscription(model, nickName, false);
+                while (true)
+                {
+                    BasicDeliverEventArgs basicDeliveryEventArgs = subscription.Next();
+                    string messageContent = Encoding.UTF8.GetString(basicDeliveryEventArgs.Body);
+                    Message message = JsonConvert.DeserializeObject<Message>(messageContent);
+                    Wall.Dispatcher.BeginInvoke(new Action(delegate ()
+                    {
+                        TextBlock item = new TextBlock() { Width = 570, TextWrapping = TextWrapping.Wrap, Text = $"{message.Login}: {message.Text}" };
+                        Wall.Items.Add(item);
+                    }));
+                    //subscription.Ack(basicDeliveryEventArgs);
+                }
+            });
+        }
+
+        // нажатие кнопки отправить
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(new Message() { Login = nickName, Text = Message.Text });
+            Message.Text = String.Empty;
+        }
+        void SendMessage(Message message)
+        {
+            IModel model = GetRabbitChannel("server", "queueName", "routingKey");
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            model.BasicPublish("server", "routingKey", null, messageBodyBytes);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Client_v3.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static IConnection conn { get; set; }
+        static List<string> users = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start server");
+            conn = GetRabbitConnection();
+            IModel server = GetRabbitChannel("server", "queueName", "routingKey");
+            var subscription = new Subscription(server, "queueName", false);
+
+            while (true)
+            {
+                BasicDeliverEventArgs basicDeliveryEventArgs = subscription.Next();
+                string messageContent = Encoding.UTF8.GetString(basicDeliveryEventArgs.Body);
+                Message message = JsonConvert.DeserializeObject<Message>(messageContent);
+                Console.WriteLine(message.Login + ": " + message.Text);
+                if (message.Text == "Authorization")
+                {
+                    users.Add(message.Login);
+                }
+                else
+                {
+                    SendMessageToAll(message);
+                }
+                //subscription.Ack(basicDeliveryEventArgs);
+            }
+        }
+        static void SendMessageToAll(Message message)
+        {
+            foreach (string s in users)
+            {
+                IModel model = GetRabbitChannel(s, s, s);
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                model.BasicPublish(s, s, null, messageBodyBytes);
+            }
+        }
+
+        // Создание и конфигурирование подключения к очереди
+        private static IModel GetRabbitChannel(string exchangeName, string queueName, string routingKey)
+        {
+            IModel model = conn.CreateModel();
+            model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+            model.QueueDeclare(queueName, false, false, false, null);
+            model.QueueBind(queueName, exchangeName, routingKey, null);
+            return model;
+        }
+
+        private static IConnection GetRabbitConnection()
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            IConnection conn = factory.CreateConnection();
+            return conn;
+        }
+    }
+}
